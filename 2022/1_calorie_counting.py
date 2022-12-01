ecd09d3119f1f56b536806a91fea23a12a6b4258
Andrey Ivanov <veny.xd@gmail.com>
2022-12-01 17:22:32 +0100

    day 1

---
+++ 2022/1_calorie_counting.py
@@ -0,0 +1,21 @@
+# https://adventofcode.com/2022/day/1
+
+calories = [line.strip() for line in open('./input/1_input')]
+
+sum_elves = []
+one_elf = 0
+
+for item in calories:
+    if item == '':
+        sum_elves.append(one_elf)
+        one_elf = 0
+    else:
+        one_elf += int(item)
+
+sum_elves.append(one_elf)
+
+# Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+print(max(sum_elves))
+
+# Find the top three Elves carrying the most Calories. How many Calories are those Elves carrying in total?
+print(sum(sorted(sum_elves, reverse=True)[0:3]))
