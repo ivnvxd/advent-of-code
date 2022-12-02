9126b53ad32efecb747dadc2c229dbb677e3245d
Andrey Ivanov <veny.xd@gmail.com>
2022-12-02 14:23:54 +0100

    2022 day 2

---
+++ 2022/02/rock_paper_scissors.py
@@ -0,0 +1,40 @@
+# https://adventofcode.com/2022/day/2
+
+"""
+opponent: A: Rock; B: Paper; C: Scissors
+response: X: Rock; Y: Paper; Z: Scissors
+"""
+
+strategy = [line.strip() for line in open('input')]
+
+points = 0
+sec_points = 0
+
+scores = {'X': 1, 'Y': 2, 'Z': 3}
+outcome = {'lost': 0, 'draw': 3, 'won': 6}
+variations = {('A', 'X'): 'draw', ('A', 'Y'): 'won', ('A', 'Z'): 'lost',
+              ('B', 'X'): 'lost', ('B', 'Y'): 'draw', ('B', 'Z'): 'won',
+              ('C', 'X'): 'won', ('C', 'Y'): 'lost', ('C', 'Z'): 'draw',
+              }
+
+sec_scores = {'X': 0, 'Y': 3, 'Z': 6}
+sec_outcome = {'rock': 1, 'paper': 2, 'scissors': 3}
+sec_variations = {('A', 'X'): 'scissors', ('A', 'Y'): 'rock', ('A', 'Z'): 'paper',
+                  ('B', 'X'): 'rock', ('B', 'Y'): 'paper', ('B', 'Z'): 'scissors',
+                  ('C', 'X'): 'paper', ('C', 'Y'): 'scissors', ('C', 'Z'): 'rock',
+                  }
+
+for item in strategy:
+    opponent, response = item.split()
+
+    points += scores[response]
+    points += outcome[variations[(opponent, response)]]
+
+    sec_points += sec_scores[response]
+    sec_points += sec_outcome[sec_variations[(opponent, response)]]
+
+# What would your total score be if everything goes exactly according to your strategy guide?
+print(points)
+
+# Following the Elf's instructions for the second column, what would your total score be if everything goes exactly according to your strategy guide?
+print(sec_points)
--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Advent of Code
 
 My solutions to [Advent of Code](https://adventofcode.com/) puzzles.
+
+---
+
+<details><summary>2022</summary>
+
+* [Day 1: Calorie Counting](2022/01)
+* [Day 2: Rock Paper Scissors](2022/02)
+
+</details>
+
+---
+
+<details><summary>2021</summary>
+
+* [Day 1: Sonar Sweep](2021/01)
+* [Day 2: Dive!](2021/02)
+
+</details>
