e5c20c0d45ca3cff6d1ce14ba599d9ba7ee7a18f
Andrey Ivanov <veny.xd@gmail.com>
2022-12-05 12:11:31 +0100

    2022 day 3

---
+++ 2022/03/rucksack_reorganization.py
@@ -0,0 +1,45 @@
+# https://adventofcode.com/2022/day/3
+
+
+def count(letter):
+    if letter.isupper():
+        return ord(letter) - ord('A') + 1 + 26
+    else:
+        return ord(letter) - ord('a') + 1
+
+
+rucksacks = [line.strip() for line in open('input')]
+
+points = 0
+group_points = 0
+
+for item in rucksacks:
+    first = item[:len(item)//2]
+    second = item[len(item)//2:]
+
+    common = set(first).intersection(second)
+    common = next(iter(common))
+
+    # print(f'item: {item} — first: {first}, second: {second}, common: {common}')
+
+    points += count(common)
+
+i = 0
+
+while i < len(rucksacks):
+    first = rucksacks[i]
+    second = rucksacks[i+1]
+    third = rucksacks[i+2]
+
+    common = set(first).intersection(second).intersection(third)
+    common = next(iter(common))
+
+    group_points += count(common)
+
+    i += 3
+
+# Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types?
+print(points)
+
+# Find the item type that corresponds to the badges of each three-Elf group. What is the sum of the priorities of those item types?
+print(group_points)
